e2167822ee0b855b9739c02fdb76203990cde9cf
Shady Boshra <shadyboshra2012@hotmail.com>
2019-08-26 13:55:33 +0200

    Website going to work

---
+++ .dart_tool/build/generated/shady_boshra_portfolio/lib/main.ddc.js
@@ -0,0 +1,349 @@
+define(['dart_sdk', 'packages/flutter_web/material', 'packages/flutter_web/animation'], function(dart_sdk, packages__flutter_web__material, packages__flutter_web__animation) {
+  'use strict';
+  const core = dart_sdk.core;
+  const _interceptors = dart_sdk._interceptors;
+  const dart = dart_sdk.dart;
+  const dartx = dart_sdk.dartx;
+  const app = packages__flutter_web__material.src__material__app;
+  const theme_data = packages__flutter_web__material.src__material__theme_data;
+  const colors = packages__flutter_web__material.src__material__colors;
+  const scaffold = packages__flutter_web__material.src__material__scaffold;
+  const app_bar = packages__flutter_web__material.src__material__app_bar;
+  const widget_inspector = packages__flutter_web__animation.src__widgets__widget_inspector;
+  const framework = packages__flutter_web__animation.src__widgets__framework;
+  const text = packages__flutter_web__animation.src__widgets__text;
+  const basic = packages__flutter_web__animation.src__widgets__basic;
+  const flex = packages__flutter_web__animation.src__rendering__flex;
+  const binding = packages__flutter_web__animation.src__widgets__binding;
+  const main = Object.create(dart.library);
+  let JSArrayOfWidget = () => (JSArrayOfWidget = dart.constFn(_interceptors.JSArray$(framework.Widget)))();
+  const CT = Object.create(null);
+  dart.defineLazy(CT, {
+    get C2() {
+      return C2 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "title",
+        column: 24,
+        line: 14,
+        file: null
+      });
+    },
+    get C1() {
+      return C1 = dart.constList([C2 || CT.C2], widget_inspector._Location);
+    },
+    get C0() {
+      return C0 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C1 || CT.C1,
+        name: null,
+        column: 13,
+        line: 14,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C5() {
+      return C5 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "title",
+        column: 7,
+        line: 10,
+        file: null
+      });
+    },
+    get C6() {
+      return C6 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "theme",
+        column: 7,
+        line: 11,
+        file: null
+      });
+    },
+    get C7() {
+      return C7 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "home",
+        column: 7,
+        line: 14,
+        file: null
+      });
+    },
+    get C4() {
+      return C4 = dart.constList([C5 || CT.C5, C6 || CT.C6, C7 || CT.C7], widget_inspector._Location);
+    },
+    get C3() {
+      return C3 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C4 || CT.C4,
+        name: null,
+        column: 12,
+        line: 9,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C10() {
+      return C10 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "data",
+        column: 21,
+        line: 31,
+        file: null
+      });
+    },
+    get C9() {
+      return C9 = dart.constList([C10 || CT.C10], widget_inspector._Location);
+    },
+    get C8() {
+      return C8 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C9 || CT.C9,
+        name: null,
+        column: 16,
+        line: 31,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C13() {
+      return C13 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "title",
+        column: 9,
+        line: 31,
+        file: null
+      });
+    },
+    get C12() {
+      return C12 = dart.constList([C13 || CT.C13], widget_inspector._Location);
+    },
+    get C11() {
+      return C11 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C12 || CT.C12,
+        name: null,
+        column: 15,
+        line: 30,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C16() {
+      return C16 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "data",
+        column: 15,
+        line: 54,
+        file: null
+      });
+    },
+    get C15() {
+      return C15 = dart.constList([C16 || CT.C16], widget_inspector._Location);
+    },
+    get C14() {
+      return C14 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C15 || CT.C15,
+        name: null,
+        column: 13,
+        line: 53,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C19() {
+      return C19 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "mainAxisAlignment",
+        column: 11,
+        line: 51,
+        file: null
+      });
+    },
+    get C20() {
+      return C20 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "children",
+        column: 11,
+        line: 52,
+        file: null
+      });
+    },
+    get C18() {
+      return C18 = dart.constList([C19 || CT.C19, C20 || CT.C20], widget_inspector._Location);
+    },
+    get C17() {
+      return C17 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C18 || CT.C18,
+        name: null,
+        column: 16,
+        line: 36,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C23() {
+      return C23 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "child",
+        column: 9,
+        line: 36,
+        file: null
+      });
+    },
+    get C22() {
+      return C22 = dart.constList([C23 || CT.C23], widget_inspector._Location);
+    },
+    get C21() {
+      return C21 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C22 || CT.C22,
+        name: null,
+        column: 13,
+        line: 33,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C26() {
+      return C26 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "appBar",
+        column: 7,
+        line: 30,
+        file: null
+      });
+    },
+    get C27() {
+      return C27 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: null,
+        name: "body",
+        column: 7,
+        line: 33,
+        file: null
+      });
+    },
+    get C25() {
+      return C25 = dart.constList([C26 || CT.C26, C27 || CT.C27], widget_inspector._Location);
+    },
+    get C24() {
+      return C24 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C25 || CT.C25,
+        name: null,
+        column: 12,
+        line: 29,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    },
+    get C29() {
+      return C29 = dart.constList([], widget_inspector._Location);
+    },
+    get C28() {
+      return C28 = dart.const({
+        __proto__: widget_inspector._Location.prototype,
+        parameterLocations: C29 || CT.C29,
+        name: null,
+        column: 23,
+        line: 3,
+        file: "org-dartlang-app:///packages/shady_boshra_portfolio/main.dart"
+      });
+    }
+  });
+  let C2;
+  let C1;
+  let C0;
+  let C5;
+  let C6;
+  let C7;
+  let C4;
+  let C3;
+  main.MyApp = class MyApp extends framework.StatelessWidget {
+    build(context) {
+      return new app.MaterialApp.new({title: "Flutter Demo", theme: theme_data.ThemeData.new({primarySwatch: colors.Colors.blue}), home: new main.MyHomePage.new({title: "Flutter Demo Home Page", $creationLocationd_0dea112b090073317d4: C0 || CT.C0}), $creationLocationd_0dea112b090073317d4: C3 || CT.C3});
+    }
+  };
+  (main.MyApp.new = function(opts) {
+    let $creationLocationd_0dea112b090073317d4 = opts && '$creationLocationd_0dea112b090073317d4' in opts ? opts.$creationLocationd_0dea112b090073317d4 : null;
+    main.MyApp.__proto__.new.call(this, {$creationLocationd_0dea112b090073317d4: $creationLocationd_0dea112b090073317d4});
+    ;
+  }).prototype = main.MyApp.prototype;
+  dart.addTypeTests(main.MyApp);
+  dart.setMethodSignature(main.MyApp, () => ({
+    __proto__: dart.getMethods(main.MyApp.__proto__),
+    build: dart.fnType(framework.Widget, [framework.BuildContext])
+  }));
+  dart.setLibraryUri(main.MyApp, "package:shady_boshra_portfolio/main.dart");
+  let C10;
+  let C9;
+  let C8;
+  let C13;
+  let C12;
+  let C11;
+  let C16;
+  let C15;
+  let C14;
+  let C19;
+  let C20;
+  let C18;
+  let C17;
+  let C23;
+  let C22;
+  let C21;
+  let C26;
+  let C27;
+  let C25;
+  let C24;
+  main.MyHomePage = class MyHomePage extends framework.StatelessWidget {
+    get title() {
+      return this[title$];
+    }
+    set title(value) {
+      super.title = value;
+    }
+    build(context) {
+      return new scaffold.Scaffold.new({appBar: new app_bar.AppBar.new({title: new text.Text.new(this.title, {$creationLocationd_0dea112b090073317d4: C8 || CT.C8}), $creationLocationd_0dea112b090073317d4: C11 || CT.C11}), body: new basic.Center.new({child: new basic.Column.new({mainAxisAlignment: flex.MainAxisAlignment.center, children: JSArrayOfWidget().of([new text.Text.new("Hello, World!", {$creationLocationd_0dea112b090073317d4: C14 || CT.C14})]), $creationLocationd_0dea112b090073317d4: C17 || CT.C17}), $creationLocationd_0dea112b090073317d4: C21 || CT.C21}), $creationLocationd_0dea112b090073317d4: C24 || CT.C24});
+    }
+  };
+  (main.MyHomePage.new = function(opts) {
+    let key = opts && 'key' in opts ? opts.key : null;
+    let title = opts && 'title' in opts ? opts.title : null;
+    let $creationLocationd_0dea112b090073317d4 = opts && '$creationLocationd_0dea112b090073317d4' in opts ? opts.$creationLocationd_0dea112b090073317d4 : null;
+    this[title$] = title;
+    main.MyHomePage.__proto__.new.call(this, {key: key, $creationLocationd_0dea112b090073317d4: $creationLocationd_0dea112b090073317d4});
+    ;
+  }).prototype = main.MyHomePage.prototype;
+  dart.addTypeTests(main.MyHomePage);
+  const title$ = Symbol("MyHomePage.title");
+  dart.setMethodSignature(main.MyHomePage, () => ({
+    __proto__: dart.getMethods(main.MyHomePage.__proto__),
+    build: dart.fnType(framework.Widget, [framework.BuildContext])
+  }));
+  dart.setLibraryUri(main.MyHomePage, "package:shady_boshra_portfolio/main.dart");
+  dart.setFieldSignature(main.MyHomePage, () => ({
+    __proto__: dart.getFields(main.MyHomePage.__proto__),
+    title: dart.finalFieldType(core.String)
+  }));
+  let C29;
+  let C28;
+  main.main = function main$() {
+    return binding.runApp(new main.MyApp.new({$creationLocationd_0dea112b090073317d4: C28 || CT.C28}));
+  };
+  dart.trackLibraries("packages/shady_boshra_portfolio/main", {
+    "package:shady_boshra_portfolio/main.dart": main
+  }, {
+  }, '{"version":3,"sourceRoot":"","sources":["main.dart"],"names":[],"mappings":";;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;UAO4B;AACxB,YAAO,iCACE,uBACA,yCACiB,4BAElB,gCAAkB;IAE5B;;;;;;EACF;;;;;;;;;;;;;;;;;;;;;;;;;;;;IAKe;;;;;;UAGa;AAIxB,YAAO,oCACG,+BACC,kBAAK,mIAER,6BAGG,yCAegC,yCACnB,sBAChB,kBACE;IAMZ;;;QAxCgB;QAAU;;;AAAU,mDAAW,GAAG;;EAAC;;;;;;;;;;;;;;;AAjBtC,0BAAO;EAAQ","file":"main.ddc.js"}');
+  // Exports:
+  return {
+    main: main
+  };
+});
+
+//# sourceMappingURL=main.ddc.js.map
